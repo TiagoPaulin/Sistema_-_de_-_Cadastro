cbdc7091960807fd4e357535af2c2016cc758990
Tiago Paulin <127238987+TiagoPaulin@users.noreply.github.com>
2024-03-11 17:19:06 -0300

    Delete Teste.java

--- Teste.java
+++
@@ -1,6 +0,0 @@
-public class Teste {
-    public static void main(String[] args) {
-            Usuario usuario = new Usuario("Tiago Vieira Paulin", "thiago.vmp15@gmail", "18", "1,80");
-        System.out.println(usuario);
-    }
-}
